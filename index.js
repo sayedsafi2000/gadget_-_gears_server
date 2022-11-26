ca07a99aab81eefc0d7fc3f79fb7e9c75a778aa4
sayed_safi2000 <safiuddin102030@gmail.com>
2022-11-26 06:28:35 +0600

    added buyers api

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ app.get("/", async (req, res) => {
 });
 
 //mongo db atlas
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ldps5dz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// jwt function 
 async function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -37,12 +36,13 @@ async function verifyJWT(req, res, next) {
 
 async function run() {
     try {
+        // all collection of mongodb 
         const serviceCollection = client.db("gadgetAndgears").collection("services");
         const productsCollection = client.db("gadgetAndgears").collection("products");
         const usersCollection = client.db("gadgetAndgears").collection("users");
         const bookingCollection = client.db("gadgetAndgears").collection("booking");
 
-
+// jwt token api 
         app.get("/jwt", async (req, res) => {
             const email = req.query.email;
             const query = { email: email }
@@ -54,57 +54,84 @@ async function run() {
             console.log(user)
             res.status(403).send({ accessToken: "" })
         })
-
+// get all category from collection 
         app.get("/category", async (req, res) => {
             const query = {}
             const result = await serviceCollection.find(query).toArray();
             res.send(result)
         });
+// get all products by category from collection 
         app.get("/service/:category", async (req, res) => {
             const category = req.params.category;
             const query = ({ category })
             const result = await serviceCollection.find(query).toArray();
             res.send(result)
         });
+// category posted by seller from client site 
         app.post("/category/allCategories", async (req, res) => {
             const query = req.body;
             const result = await productsCollection.insertOne(query);
             res.send(result)
         });
+        // get product by matching category 
         app.get("/category/:category", async (req, res) => {
             const category = req.params.category
             let query = { category };
-            // if (req.params.category) {
-            //     query = {
-            //         category: req.query.category
-            //     }
-            // }
             const cursor = productsCollection.find(query);
             const product = await cursor.toArray();
             res.send(product);
         });
+        // create user by login google and save on user collections 
+        app.put("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email};
+            options = {upsert: true};
+            const updatedDoc={
+                $set:{
+                    name:user.name,
+                    email:user.email,
+                    userType: user.userType,
+                }
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+        // create user by login firebase authentications and save on user collections 
         app.post("/users", async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+        // get all user from user collections 
         app.get("/users", async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
         });
+        // make a api for admin role user 
         app.get("/users/admin/:email", async (req, res) => {
             const email = req.params.email;
             const query = { email };
             const user = await usersCollection.findOne(query);
             res.send({ isAdmin: user?.role === "admin" });
         });
+        // make a api for buyer role user 
+        app.get("/users/buyer/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.userType === "Buyer" });
+        });
+        // make a api for seller role user 
         app.get("/users/seller/:email", async (req, res) => {
             const email = req.params.email;
             const query = { email };
             const user = await usersCollection.findOne(query);
-            res.send({ isSeller: user?.role === "seller" });
+            console.log(user)
+            res.send({ isSeller: user?.userType === "Seller" });
         })
+        // create api for verify user and it can be done if the user is admin 
         app.put("/users/admin/:id", verifyJWT, async (req, res) => {
             const decodedEmail = req.decoded.email;
             const query = { email: decodedEmail };
@@ -123,32 +150,19 @@ async function run() {
             }
             const result = await usersCollection.updateOne(filter, updatedDoc, options)
         });
+// delet a user(seller or buyer) by admin 
         app.delete("/users/admin/:id", verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = ({ _id: ObjectId(id) })
             const result = await usersCollection.deleteOne(query);
             if (result.deletedCount === 1) {
-                console.log("Successfully deleted one document.");
+                console.log("Successfully deleted one user.");
             } else {
                 console.log("No documents matched the query. Deleted 0 documents.");
             }
             res.send(result)
         });
-        // app.put("/users/salers:id",async(req,res)=>{
-        //     const id =req.params.id;
-        //     const filter = {_id: ObjectId(id)};
-        //     const options = {upsert: true}
-        //     const updatedDoc={
-        //         $set: {
-        //             role: "seller"
-        //         }
-        //     }
-        //     const result= await usersCollection.updateOne(filter,updatedDoc,options);
-        //     res.send(result);
-        // });
-
-
-
+// api for for post booking info from client site 
         app.post("/booking", async (req, res) => {
             const user = req.body;
             const result = await bookingCollection.insertOne(user);
